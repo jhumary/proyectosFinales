fdfbddb7b5643b4e12060145b6ea02c9f2402f9c
Jhumari Soto <Jhumari Soto@TOSHIBA>
2015-06-16 22:43:59 -0500

    Primer comit

--- JavaApplication34/src/JavaApplication34.java
+++ JavaApplication34/src/JavaApplication34.java
@@ -1,4 +1,7 @@
 
+import javax.swing.JOptionPane;
+
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -16,6 +19,7 @@ public class JavaApplication34 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        JOptionPane.showMessageDialog(null,"hola mundo");
     }
     
 }
